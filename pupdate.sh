338b2b6b0d0843f1edd54e6b3b181caab332bfa2
mc9662 <42133732+mc9662@users.noreply.github.com>
2018-09-08 16:14:41 +0100

    Create pupdate.sh

---
+++ pupdate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TICK="[\e[32m ✔ \e[0m]"
+
+echo -e " \e[1m This script will download and update @mc9662 adlists.list \e[0m"
+sleep 1
+echo -e "\n"
+
+if [ "$(id -u)" != "0" ] ; then
+        echo "This script requires root permissions. Please run this as root!"
+        exit 2
+fi
+
+cp /etc/pihole/adlists.list /etc/pihole/adlists.list.bak
+list="$(grep "^(\^|.*\$$" /etc/pihole/adlists.list)"
+list+="
+$(wget -qO - https://raw.githubusercontent.com/mc9662/pihole/master/adlists.list)"
+LC_COLLATE=C sort -u <<< "$list" | grep -v "^#" | grep -v '^[[:space:]]*$' | sudo tee /etc/pihole/adlists.list
+killall -SIGHUP pihole-FTL
+wait
+echo -e " ${TICK} \e[32m Pi-hole's adlist updated \e[0m"
+echo -e " ${TICK} \e[32m Done! \e[0m"
+
+exit
