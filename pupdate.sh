aafac1125dcce8abc93f0f2f5ac6a5ff4db4390f
mc9662 <42133732+mc9662@users.noreply.github.com>
2018-09-09 11:41:00 +0100

    Update pupdate.sh

--- pupdate.sh
+++ pupdate.sh
@@ -10,15 +10,16 @@ if [ "$(id -u)" != "0" ] ; then
         echo "This script requires root permissions. Please run this as root!"
         exit 2
 fi
-echo -e " ${TICK} \e[32m Backing Up Adlists.list \e[0m"
+echo -e " ${TICK} \e[32m Backing Up adlists.list to /etc/pihole/adlists.list.bak \e[0m"
 cp /etc/pihole/adlists.list /etc/pihole/adlists.list.bak
 echo -e " ${TICK} \e[32m Downloading @mc9662 adlists.list! \e[0m"
 list="$(grep "^(\^|.*\$$" /etc/pihole/adlists.list)"
 list+="
 $(wget -qO - https://raw.githubusercontent.com/mc9662/pihole/master/adlists.list)"
-LC_COLLATE=C sort -u <<< "$list" | grep -v "^#" | grep -v '^[[:space:]]*$' | sudo tee /etc/pihole/adlists.list >/dev/null
+LC_COLLATE=C sort -u <<< "$list" | grep -v "^#" | grep -v '^[[:space:]]*$' | sudo tee /etc/pihole/adlists.list
 wait
 echo -e " ${TICK} \e[32m Pi-hole's adlist updated \e[0m"
 echo -e " ${TICK} \e[32m Done! \e[0m"
 
 exit
+
